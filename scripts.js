4c7815dd704da55e9ed74b8400fa864db1a4d630
xchbx <ceina.ellison@gmail.com>
2024-10-05 10:24:21 -0400

    styled items displayed using grid css

--- scripts.js
+++ scripts.js
@@ -7,6 +7,14 @@ for (let i = 0; i < data.length; i += 1) {
 	// create a new div element and give it a class name
 	const newDiv = document.createElement("div");
 	newDiv.className = "item"
+
+    // create h2
+    const name = document.createElement("H")
+    // add a class name to the h2
+    name.className = "name"
+    name.innerText = data[i].name
+    newDiv.appendChild(name)
+    
 	// create an image element
 	const img = document.createElement("img");
 	// this will change each time we go through the loop. Can you explain why?
@@ -21,6 +29,8 @@ for (let i = 0; i < data.length; i += 1) {
 	const desc = document.createElement("P")
 	// give the paragraph text from the data
 	desc.innerText = data[i].desc
+    //class name
+    desc.className = "desc"
 	// append the paragraph to the div
 	newDiv.appendChild(desc)
 	// do the same thing for price
@@ -29,10 +39,13 @@ for (let i = 0; i < data.length; i += 1) {
 	newDiv.appendChild(price)
 	// Make a button 
 	const button = document.createElement("button")
+    // add a class name to the button
+    button.className = "add-to-cart"
 	// add an  id name to the button
 	button.dataset.id = data[i].name
 	// creates a custom attribute called data-price. That will hold price for each element in the button
 	button.dataset.price = data[i].price
 	button.innerHTML = "Add to Cart"
 	newDiv.appendChild(button)
+
 }
